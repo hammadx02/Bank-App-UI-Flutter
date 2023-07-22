a40f4e852ef96a95fa467cca04169c42c8639c3f
hammadx02 <hammadaali02@gmail.com>
2023-07-23 03:34:30 +0500

    initial commit

---
+++ lib/components/circle.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+import '../constents/constents.dart';
+
+class Circle extends StatelessWidget {
+  final circleIcon;
+  final circleText;
+  const Circle({super.key, required this.circleIcon, required this.circleText});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 5.0),
+        child: Container(
+          height: 80,
+          width: 80,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            shape: BoxShape.circle,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.1),
+                spreadRadius: 2,
+                blurRadius: 5,
+                offset: const Offset(1, 3),
+              ),
+            ],
+          ),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Icon(
+                  circleIcon,
+                  color: themeColor,
+                  size: 30,
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                Center(
+                  child: Text(
+                    circleText,
+                    style: const TextStyle(
+                        color: themeColor,
+                        fontSize: 10,
+                        fontFamily: 'Poppins Medium'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,190 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:med_app_ui/components/circle.dart';
+
+import '../constents/constents.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blue[50],
+        appBar: AppBar(
+          leading: const Icon(
+            FontAwesomeIcons.bars,
+            color: themeColor,
+          ),
+          actions: const [
+            Padding(
+              padding: EdgeInsets.only(right: 30),
+              child: Icon(
+                FontAwesomeIcons.bell,
+                color: themeColor,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                right: 10.0,
+              ),
+              child: Icon(
+                FontAwesomeIcons.solidUser,
+                color: themeColor,
+              ),
+            )
+          ],
+          elevation: 0,
+          backgroundColor: Colors.blue[50],
+          title: Center(
+            child: RichText(
+              text: const TextSpan(
+                text: 'Medica',
+                style: TextStyle(
+                    color: themeColor,
+                    fontSize: 24,
+                    fontFamily: 'Poppins Bold'),
+                children: [
+                  TextSpan(
+                    text: ' Reminder',
+                    style: TextStyle(
+                        fontFamily: 'Poppins Regular',
+                        fontSize: 24,
+                        color: themeColor),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        body: Column(
+          children: [
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 15.0, vertical: 10),
+              child: Container(
+                height: 180,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.1),
+                        spreadRadius: 2,
+                        blurRadius: 5,
+                        offset: const Offset(1, 3),
+                      ),
+                    ],
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(40),
+                    color: themeColor),
+                child: Padding(
+                  padding:
+                      const EdgeInsets.only(left: 30.0, top: 30, bottom: 15),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      RichText(
+                        text: const TextSpan(
+                          text: 'Good Afternoon,\n',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                              fontFamily: 'Poppins Medium'),
+                          children: [
+                            TextSpan(
+                              text: 'Hammad Ali.',
+                              style: TextStyle(
+                                  fontFamily: 'Poppins Regular',
+                                  fontSize: 18,
+                                  color: Colors.white),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(right: 30, top: 10),
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            suffixIcon: const Icon(
+                              FontAwesomeIcons.magnifyingGlass,
+                              size: 16,
+                              color: Colors.grey,
+                            ),
+                            hintText: 'Search',
+                            hintStyle: const TextStyle(
+                                color: Colors.grey,
+                                fontSize: 16,
+                                fontFamily: 'Poppins Regular'),
+                            fillColor: Colors.white,
+                            filled: true,
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15),
+                              borderSide: const BorderSide(color: Colors.black),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: const BorderSide(color: Colors.black),
+                              borderRadius: BorderRadius.circular(15),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 15.0, vertical: 10),
+              child: Container(
+                height: 120,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(30),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.1),
+                      spreadRadius: 2,
+                      blurRadius: 5,
+                      offset: const Offset(1, 3),
+                    ),
+                  ],
+                ),
+                child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: 3,
+                    ),
+                    Circle(
+                        circleIcon: FontAwesomeIcons.clock,
+                        circleText: '      Med\nReminders'),
+                    Circle(
+                        circleIcon: FontAwesomeIcons.calendarCheck,
+                        circleText: 'Appointments'),
+                    Circle(
+                        circleIcon: FontAwesomeIcons.bookMedical,
+                        circleText: 'Medical\nRecords'),
+                    Circle(
+                        circleIcon: FontAwesomeIcons.peopleGroup,
+                        circleText: '   Patient\nCaretaker'),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
